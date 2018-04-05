ead339fdd516c6f1ae7f0b77a1e51c256e2d83dc
Kerrick Woyshner <kerrickwoyshner@CWRU-Sierra-10ddb1bb4306.attlocal.net>
2018-04-05 03:55:02 -0400

    key initialization on JS files

--- liri.js
+++ liri.js
@@ -1,4 +1,25 @@
+var keys = require("./keys.js");
 require("dotenv").config();
+
+var Spotify = function() {
+};
+var Twitter = function() {
+};
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var action = process.argv[2];
+
+function myTweets() {
+    if(action === "my-tweets") {
+        Twitter.get('statuses/user_timeline', function(error, tweets, response){
+            if (!error) {
+              console.log("tweets");
+            }
+          });
+    } else {
+        console.log("bug fix");
+    }
+}
+
+myTweets();
