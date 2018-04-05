d96c60e0a3050e3c3261478dd2556aff4670acb8
Kerrick Woyshner <kerrickwoyshner@CWRU-Sierra-10ddb1bb4306.attlocal.net>
2018-04-05 04:07:25 -0400

    twitter api functionality

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 var keys = require("./keys.js");
+var Twitter = require('twitter');
 require("dotenv").config();
 
 var Spotify = function() {
@@ -7,19 +8,25 @@ var Twitter = function() {
 };
 
 var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var T = new Twitter(keys.twitter);
 var action = process.argv[2];
 
 function myTweets() {
     if(action === "my-tweets") {
-        Twitter.get('statuses/user_timeline', function(error, tweets, response){
+        var params = {
+            q: '@slice_served',
+            count: 2,
+            result_type: 'recent',
+            lang: 'en'
+          }
+        T.get('statuses/user_timeline', params, function(error, tweets, response){
             if (!error) {
-              console.log("tweets");
+              console.log(tweets);
             }
           });
     } else {
         console.log("bug fix");
     }
-}
+};
 
 myTweets();
