348af8e409b56bbe9cd9162d870e8330b8e5ff57
Kerrick Woyshner <kerrickwoyshner@CWRU-Sierra-10ddb1bb4306.attlocal.net>
2018-04-05 00:31:31 -0400

    key initialization

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
+
+exports.spotify = {
+  id: process.env.7883301db58041be8ae2365d3ad590e5,
+  secret: process.env.73696030967840abbb1044f9ff774688
+};
