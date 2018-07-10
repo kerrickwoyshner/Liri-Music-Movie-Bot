e54366243322ba34e365ccccf7a51551c9b40649
kerrickwoyshner <kerrick.woyshner@gmail.com>
2018-07-10 16:09:31 -0400

    documentation added on liri.js & keys.js

--- liri.js
+++ liri.js
@@ -1,107 +1,149 @@
+// DEPENDENCIES
+// =====================================
+// Read and set environment variables
 require("dotenv").config();
-var fs = require("fs");
+
+// Import the Twitter NPM package.
+var Twitter = require("twitter");
+
+// Import the node-spotify-api NPM package.
+var Spotify = require("node-spotify-api");
+
+// Import the API keys
+var keys = require("./keys");
+
+// Import the request npm package.
 var request = require("request");
-var keys = require("./keys.js");
 
-var Twitter = function () { };
-var twitter = require('twitter');
-var T = new Twitter(keys.twitter);
+// Import the FS package for read/write.
+var fs = require("fs");
 
-var Spotify = require('node-spotify-api');
+// Initialize the spotify API client using our client id and secret
 var spotify = new Spotify(keys.spotify);
 
-var action = process.argv[2];
+// FUNCTIONS
+// =====================================
 
-switch (action) {
-    case "my-tweets":
-        myTweets();
-        break;
-    case "spotify-this-song":
-        spotifyThisSong();
-        break;
-    case "movie-this":
-        movieThis();
-        break;
-    case "do-what-it-says":
-        doWhatItSays();
-        break;
+// Writes to the log.txt file
+var getArtistNames = function (artist) {
+    return artist.name;
 };
 
-function nodeRecord() {
-fs.appendFile("log.txt", "utf8", function(err) {
-    // If an error was experienced we say it.
-    if (err) {
-      console.log(err);
+// Function for running a Spotify search
+var getMeSpotify = function (songName) {
+    if (songName === undefined) {
+        songName = "What's my age again";
     }
-    // If no error is experienced, we'll log the phrase "Content Added" to our node console.
-    else {
-      console.log("Content Added!");
-    }
-  });
-};
-  
 
-function myTweets() {
+    spotify.search(
+        {
+            type: "track",
+            query: songName
+        },
+        function (err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
+                return;
+            }
+
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log(i);
+                console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+                console.log("song name: " + songs[i].name);
+                console.log("preview song: " + songs[i].preview_url);
+                console.log("album: " + songs[i].album.name);
+                console.log("-----------------------------------");
+            }
+        }
+    );
+};
+
+// Function for running a Twitter Search
+var getMyTweets = function () {
+    var client = new Twitter(keys.twitter);
+
     var params = {
-        q: '@slice_served',
-        count: 2,
-        result_type: 'recent',
-        lang: 'en'
-    }
-    request('statuses/user_timeline', params, function (error, tweets, response) {
+        screen_name: "cnn"
+    };
+    client.get("statuses/user_timeline", params, function (error, tweets, response) {
         if (!error) {
-            console.log(tweets);
-            nodeRecord();
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].created_at);
+                console.log("");
+                console.log(tweets[i].text);
+            }
         }
     });
 };
 
-function spotifyThisSong() {
-    var songName = process.argv[3];
-    var queryUrl = "https://api.spotify.com/v1/artists/" + spotify.id;
-    console.log(queryUrl);
+// Function for running a Movie Search
+var getMeMovie = function (movieName) {
+    if (movieName === undefined) {
+        movieName = "Mr Nobody";
+    }
 
-    nodeRecord();
-};
+    var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
 
-function movieThis() {
-    var movie = process.argv[3];
-    request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
-        // If the request is successful (i.e. if the response status code is 200)
+    request(urlHit, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            console.log("The movie's title is: " + JSON.parse(body));
-            // console.log("The movie came out in  " + JSON.parse(body).);
-            // console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-            // console.log("The movie's Rotten Tomatoes rating is: " + JSON.parse(body).);
-            // console.log("The movie was produced in: " + JSON.parse(body).);
-            // console.log("The movie's spoken in: " + JSON.parse(body).);
-            // console.log("The movie's plot is: " + JSON.parse(body).);
-            // console.log("The actors featured in this movie are: " + JSON.parse(body).);
-            nodeRecord();
-        } else {
-            var movie = 'Mr. Nobody'
-            request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
-                // console.log("The movie's title is: " + JSON.parse(body).);
-                // console.log("The movie came out in  " + JSON.parse(body).);
-                // console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-                // console.log("The movie's Rotten Tomatoes rating is: " + JSON.parse(body).);
-                // console.log("The movie was produced in: " + JSON.parse(body).);
-                // console.log("The movie's spoken in: " + JSON.parse(body).);
-                // console.log("The movie's plot is: " + JSON.parse(body).);
-                // console.log("The actors featured in this movie are: " + JSON.parse(body).);
-                nodeRecord();
-            }
+            var jsonData = JSON.parse(body);
+
+            console.log("Title: " + jsonData.Title);
+            console.log("Year: " + jsonData.Year);
+            console.log("Rated: " + jsonData.Rated);
+            console.log("IMDB Rating: " + jsonData.imdbRating);
+            console.log("Country: " + jsonData.Country);
+            console.log("Language: " + jsonData.Language);
+            console.log("Plot: " + jsonData.Plot);
+            console.log("Actors: " + jsonData.Actors);
+            console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
         }
-});
+    });
 };
 
-function doWhatItSays() {
+// Function for running a command based on text file
+var doWhatItSays = function () {
     fs.readFile("random.txt", "utf8", function (error, data) {
-        if (error) {
-            return console.log(error);
-        } else {
-            console.log(data);
-            nodeRecord();
+        console.log(data);
+
+        var dataArr = data.split(",");
+
+        if (dataArr.length === 2) {
+            pick(dataArr[0], dataArr[1]);
         }
-    })
+        else if (dataArr.length === 1) {
+            pick(dataArr[0]);
+        }
+    });
 };
+
+// Function for determining which command is executed
+var pick = function (caseData, functionData) {
+    switch (caseData) {
+        case "my-tweets":
+            getMyTweets();
+            break;
+        case "spotify-this-song":
+            getMeSpotify(functionData);
+            break;
+        case "movie-this":
+            getMeMovie(functionData);
+            break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+        default:
+            console.log("LIRI doesn't know that");
+    }
+};
+
+// Function which takes in command line arguments and executes correct function accordingly
+var runThis = function (argOne, argTwo) {
+    pick(argOne, argTwo);
+};
+
+// MAIN PROCESS
+// =====================================
+runThis(process.argv[2], process.argv[3]);
